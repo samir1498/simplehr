bce816f8ffcb8655e5c7dea13960907c05b939bb
samir1498 <bettahar.samir@outlook.com>
2026-03-04 08:59:30 +0100

    test(employee-service): remove unused assertion import

--- services/employee-service/src/test/java/com/samir/simplehr/employee/domain/model/EmployeeTest.java
+++ services/employee-service/src/test/java/com/samir/simplehr/employee/domain/model/EmployeeTest.java
@@ -1,7 +1,6 @@
 package com.samir.simplehr.employee.domain.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.math.BigDecimal;
 import java.time.Instant;
